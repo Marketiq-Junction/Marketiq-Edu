9022cad8abba310f0941b7a8dff097d853bb795d
Sanadn12 <ni.sanad1433@gmail.com>
2024-11-05 18:32:09 +0530

    done with home page without resposnive and images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,28 @@ import React from 'react'
 import Navbar from '../components/navbar/navbar'
 import HeroHome from '../components/home/hero/heroHome'
 import Footer from '../components/footer/footer'
+import Mission from '@/components/home/mission/mission'
+import Vision from '@/components/home/vision/vision'
+import Chooseus from '@/components/home/chooseus/Chooseus'
+import Courses from '@/components/home/courses/courses'
+import PayAfterPlacement from '@/components/payafterplacement/payafterplacement'
+import StudentExperience from '@/components/studentexperience/studentexperience'
+import Career from '@/components/career/career'
+import Join from '@/components/join/join'
 
 const page = () => {
   return (
     <div>
       <Navbar/>
       <HeroHome/>
+      <Mission/>
+      <Vision/>
+      <Chooseus/>
+      <Courses/>
+      <PayAfterPlacement/>
+      <StudentExperience/>
+      <Career/>
+      <Join/>
       <Footer/>
       
     </div>
